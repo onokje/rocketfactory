4a3f2d98777c016e44a3e81d466d1a1bd68b65c8
onok <onokje@gmail.com>
2019-05-20 18:59:31 +0200

    Updates

---
+++ precache-manifest.7420870ddefae374a001223aec9a9ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aed7a9137f855c315a6e85291eb4ed2",
+    "url": "/rocketfactory/static/media/map.4aed7a91.jpeg"
+  },
+  {
+    "revision": "0dc0b10ac5133b315bc2",
+    "url": "/rocketfactory/static/js/runtime~main.0dc0b10a.js"
+  },
+  {
+    "revision": "40f9ffbcd3b92bd05995",
+    "url": "/rocketfactory/static/js/main.40f9ffbc.chunk.js"
+  },
+  {
+    "revision": "c3b86740c8caf25068bd",
+    "url": "/rocketfactory/static/js/1.c3b86740.chunk.js"
+  },
+  {
+    "revision": "40f9ffbcd3b92bd05995",
+    "url": "/rocketfactory/static/css/main.a0fa3eeb.chunk.css"
+  },
+  {
+    "revision": "a64dbec3f64005cd8899774bebe0cd5b",
+    "url": "/rocketfactory/index.html"
+  }
+];
